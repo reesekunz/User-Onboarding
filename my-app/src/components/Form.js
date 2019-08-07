5ff23ea01b6d04948993e9615f9e90b4b0036e5f
Reese Kunz <reesekunz21@gmail.com>
2019-08-07 12:36:59 -0700

    added inputs for email and password

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -4,8 +4,7 @@ import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
 
 const UserForm = props => {
-
-//inputs needed: name, email, password, terms of service (checkbox), submit button 
+  //inputs needed: name, email, password, terms of service (checkbox), submit button
   return (
     <div className="user-form">
       <h1>New User Form</h1>
@@ -13,32 +12,33 @@ const UserForm = props => {
       <Form>
         <Field placeholder="Name" type="text" name="name" />
 
-        <button type="submit"> Submit!</button>
+        <Field placeholder="Email" type="text" name="email" />
 
+        <Field placeholder="Password" type="text" name="password" />
+
+        <button type="submit"> Submit!</button>
       </Form>
     </div>
   );
-}
+};
 
-  const FormikUserForm = withFormik({
-    mapPropsToValues(values) {
-      return {
-        name: values.name || " "
+const FormikUserForm = withFormik({
+  mapPropsToValues(values) {
+    return {
+      name: values.name || " ",
+      email: values.email || " ",
+      password: values.password || " "
     };
-    },
-  
-    handleSubmit(values) {
-        console.log("form submitted", values);
-        axios
-          .post("https://reqres.in/api/users", values)
-          // with post, pass in data you're trying to post to database
-          .then(response => console.log(response))
-          .catch(error => console.log(error.response));
-      }
-    })(UserForm); // currying functions in Javascript
+  },
 
-    export default FormikUserForm;
-    
+  handleSubmit(values) {
+    console.log("form submitted", values);
+    axios
+      .post("https://reqres.in/api/users", values)
+      // with post, pass in data you're trying to post to database
+      .then(response => console.log(response))
+      .catch(error => console.log(error.response));
+  }
+})(UserForm); // currying functions in Javascript
 
-
-
+export default FormikUserForm;
