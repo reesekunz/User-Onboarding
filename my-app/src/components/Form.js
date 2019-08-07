c66f9d378a5b6a3dcfaf2a5a28d8d8130d125c64
Reese Kunz <reesekunz21@gmail.com>
2019-08-07 12:14:44 -0700

    set up the file structure

---
+++ my-app/src/components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
+
+const UserForm = props => {
+
+    return (
+        <div>This will be the User FormikForm</div>
+    )
+
+}
+
+export default UserForm;
+
